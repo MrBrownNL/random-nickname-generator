0694602d0eb4c157956fd261ec340075f34e3824
Jeroen de Bruijn <jeroen.de.bruijn@brightalley.nl>
2021-08-14 17:48:45 +0200

    Add function that returns a unique nickname and keeps a list with given names

--- index.php
+++ index.php
@@ -4,7 +4,14 @@ use MrBrownNL\RandomNicknameGenerator\RandomNicknameGenerator;
 
 require 'vendor/autoload.php';
 
+$start = microtime(true);
+
 $nickName = new RandomNicknameGenerator(["useAdjective" => false, "postfix" => ["maximumValue" => 999]]);
 
-echo $nickName->getNumberOfPossibleUniqueNicknames() . PHP_EOL;
-echo $nickName->generate() . PHP_EOL;
+//echo $nickName->getNumberOfPossibleUniqueNicknames() . PHP_EOL;
+
+echo $nickName->generateUnique() . PHP_EOL;
+
+$time_elapsed_secs = microtime(true) - $start;
+
+echo "Time elapsed: " . $time_elapsed_secs . " seconds." . PHP_EOL;
--- src/RandomNicknameGenerator.php
+++ src/RandomNicknameGenerator.php
@@ -2,6 +2,8 @@
 
 namespace MrBrownNL\RandomNicknameGenerator;
 
+use Exception;
+
 class RandomNicknameGenerator
 {
     private $config;
@@ -10,36 +12,107 @@ class RandomNicknameGenerator
 
     private $names;
 
+    private $uniquelyGeneratedNames;
+
     private $numberOfPossibleUniqueNicknames;
 
     public function __construct(array $options = [])
     {
-        $this->config = json_decode(json_encode(array_replace_recursive(include(__DIR__ . '/../config/config.php'), $options)));
+        $this->config = json_decode(
+            json_encode(
+                array_replace_recursive(
+                    include(__DIR__ . '/../config/config.php'),
+                    $options
+                )
+            )
+        );
+
         if ($this->config->useAdjective) {
             $this->adjectives = file(
                 __DIR__ . '/dictionaries/adjectives.txt',
                 FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES
             );
         }
-        $this->names = file(__DIR__ . '/dictionaries/names.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
-        $this->numberOfPossibleUniqueNicknames = ($this->config->postfix->maximumValue - $this->config->postfix->minimumValue + 1) *
-            count($this->names) *
-            ($this->config->useAdjective && count($this->adjectives) > 0 ? count($this->adjectives) : 1);
+        $this->names = file(
+            __DIR__ . '/dictionaries/names.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES
+        );
+
+        if (file_exists(__DIR__ . '/dictionaries/uniquelyGeneratedNames.txt')) {
+            $this->uniquelyGeneratedNames = file(
+                __DIR__ . '/dictionaries/uniquelyGeneratedNames.txt',
+                FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES
+            );
+        } else {
+            $this->uniquelyGeneratedNames = [];
+        }
+
+        $this->numberOfPossibleUniqueNicknames =
+            ($this->config->postfix->maximumValue - $this->config->postfix->minimumValue + 1)
+            * count($this->names)
+            * ($this->config->useAdjective && count($this->adjectives) > 0 ? count($this->adjectives) : 1);
     }
 
-    final public function generate(): string
+    /**
+     * Generates a non-unique random nickname
+     *
+     * @return string
+     */
+    public function generate(): string
     {
-        $nickName = ($this->config->useAdjective ? ucfirst($this->adjectives[rand(0,count($this->adjectives) - 1)]) : '') .
+        $nickname = (
+            $this->config->useAdjective
+                ? ucfirst($this->adjectives[rand(0,count($this->adjectives) - 1)])
+                : ''
+            ) .
             $this->config->separator .
             ucfirst($this->names[rand(0,count($this->names) - 1)]) .
-            ($this->config->addNumericPostfix ? rand($this->config->postfix->minimumValue, $this->config->postfix->maximumValue) : '');
+            (
+                $this->config->addNumericPostfix
+                    ? rand($this->config->postfix->minimumValue, $this->config->postfix->maximumValue)
+                    : ''
+            );
 
-        return $nickName;
+        return $nickname;
     }
 
-    final public function getNumberOfPossibleUniqueNicknames(): int
+    /**
+     * Generates a unique random nickname.
+     *
+     * @throws Exception if all unique names are given
+     *
+     * @return string
+     */
+    public function generateUnique(): string
+    {
+        if ($this->numberOfPossibleUniqueNicknames - count($this->uniquelyGeneratedNames) > 0) {
+            do {
+                $nickname = $this->generate();
+            } while (in_array($nickname, $this->uniquelyGeneratedNames));
+
+            $this->AddUniqueNicknameToList($nickname);
+
+            return $nickname;
+        }
+
+        throw new Exception('All possible nicknames are given');
+    }
+
+    public function getNumberOfPossibleUniqueNicknames(): int
     {
         return $this->numberOfPossibleUniqueNicknames;
+    }
+
+    /**
+     * @param string $nickname
+     */
+    private function AddUniqueNicknameToList($nickname): void
+    {
+        $this->uniquelyGeneratedNames[] = $nickname;
+
+        file_put_contents(
+            __DIR__ . '/dictionaries/uniquelyGeneratedNames.txt',
+            implode(PHP_EOL, $this->uniquelyGeneratedNames)
+        );
     }
 }
