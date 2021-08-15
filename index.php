fad72ee4367d416a3dcab16bb94c54557907c609
Jeroen de Bruijn <jeroen.de.bruijn@brightalley.nl>
2021-08-15 08:56:09 +0200

    Updated demo

--- index.php
+++ index.php
@@ -8,10 +8,13 @@ $start = microtime(true);
 
 $nickName = new RandomNicknameGenerator(["useAdjective" => false, "postfix" => ["maximumValue" => 999]]);
 
-//echo $nickName->getNumberOfPossibleUniqueNicknames() . PHP_EOL;
+echo "Possible unique nicknames: " . $nickName->getNumberOfPossibleUniqueNicknames() . PHP_EOL;
+echo "Available unique nicknames: " . $nickName->getNumberOfAvailableUniqueNicknames() . PHP_EOL;
 
-echo $nickName->generateUnique() . PHP_EOL;
+echo "Generated unique nickname: " . $nickName->generateUnique() . PHP_EOL;
+
+echo "Available unique nicknames: " . $nickName->getNumberOfAvailableUniqueNicknames() . PHP_EOL;
 
 $time_elapsed_secs = microtime(true) - $start;
 
-echo "Time elapsed: " . $time_elapsed_secs . " seconds." . PHP_EOL;
+echo "Execution time: " . $time_elapsed_secs . " seconds." . PHP_EOL;
